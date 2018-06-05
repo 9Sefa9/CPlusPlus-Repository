3764cc54d36f659638d93e374852cf9599ef2101
9Sefa9 <sefagoevercin@hotmail.de>
2018-06-06 00:11:09 +0200

    everything deleted!

--- SingleLinkedListAufgabe050618.c
+++ SingleLinkedListAufgabe050618.c
@@ -6,41 +6,62 @@ typedef struct singleLinkedList{
     struct node *last;
 }linkedList;
 
-struct node{
+typedef struct node{
     int data;
     struct node *next;
-};
+}n;
 //Prototypen
 void addLast(linkedList *list,int element);
 void addFirst(linkedList *list,int element);
+void printList(linkedList *list);
 
 struct node *rootPointer=NULL;
 int main(void){
     linkedList list;
-    addFirst(&list,2);
-    //addFirst(&list,3);
-
-    printf("first:%d\n",rootPointer->data);
-  //  printf("second: %d\n",rootPointer->next->data);
+    addLast(&list,2);
+    addLast(&list,3);
+    addFirst(&list,4);
+    addFirst(&list,8);
+    printf("first:%d\n",*(list.first->next));
+    printf("last: %d\n",list.last->data);
+    //printList(&list);
 }
 
 
 //Füge Element am Ende der Listei
 void addLast(linkedList *list,int element){
+    if(rootPointer == NULL){
+        list->first = malloc(sizeof(int));
+        list->first->data = element;
+        list->last = list->first;
+        rootPointer = list->last;
+        list->last->next= NULL;
+    }else {
+        list->last->next = malloc(sizeof(int));
+        list->last = list->last->next;
+        list->last->data = element;
+        list->last->next = NULL;
+    }
 }
 //Füge Element am Anfang der List
 void addFirst(linkedList *list,int element){
     if(rootPointer == NULL){
-        list->first = malloc(sizeof(struct node));
+        list->first = malloc(sizeof(int));
         list->first->data = element;
         list->last = list->first;
         rootPointer = list->first;
-
+        list->last->next= NULL;
     }else {
-        list->last->next = malloc(sizeof(struct node));
-        list->last->data = element;
-        list->last = list->last->next;
-
+        list->first->next = malloc(sizeof(int));
+        list->first = list->first->next;
+        list->first->data = element;
     }
 
+}
+void printList(linkedList *list){
+    node *tmpNode = list->first; //äquivalent zu list->first
+    while(tmpNode != NULL){
+    printf("%d\n",tmpNode->data);
+    tmpNode = tmpNode->next;
+    }
 }
