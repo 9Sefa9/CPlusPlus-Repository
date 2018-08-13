e7d69b8cb0e3a630ccbfb1708e1bc0f410bb7432
9schurke9 <sefagoevercin@hotmail.de>
2018-08-13 23:52:07 +0200

    Stack implementiert

--- C/stack.c
+++ C/stack.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "stack.h"
-
+/*
+1. Declaration can be done any number of times but definition only once.
+2. “extern” keyword is used to extend the visibility of variables/functions().
+3. Since functions are visible through out the program by default. The use of extern is not needed in function declaration/definition. Its use is redundant.
+4. When extern is used with a variable, it’s only declared not defined.
+5. As an exception, when an extern variable is declared with initialization, it is taken as definition of the variable as well.
+ * */
+test = 4;
 int main(void){
+
     Stack stack;
     stack.root = NULL;
     push(&stack,1);
@@ -16,8 +24,10 @@ int main(void){
     pop(&stack);
     pop(&stack);
     pop(&stack);
-    pop(&stack);
+    printf("extern: %i\n",test);
     print(stack);
+
+
     return 0;
 }
 void push(Stack *stack,int value){
